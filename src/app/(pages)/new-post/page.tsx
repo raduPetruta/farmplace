82291e125ffccc4456b8a2f8bea6ca42c73bd2e1
Radu Petruta <radupetruta19@gmail.com>
2024-10-05 22:43:33 +0300

    Build fix

--- src/app/(pages)/new-post/page.tsx
+++ src/app/(pages)/new-post/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-// @ts-ignore
+//@ts-expect-error
 import { v4 as uuid } from 'uuid'
 import { createPost } from './actions';
 import { useUser } from '@clerk/nextjs';
